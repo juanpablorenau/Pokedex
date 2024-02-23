55a49f9a7171f1759adb4c32de1e4ef151fed6be
jrenau <jrenau@mercadona.es>
2024-02-23 12:56:06 +0100

    develop: Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,48 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
+
+    //Testing
+    testImplementation ("junit:junit:4.13.2")
+    testImplementation ("io.mockk:mockk:1.12.4")
+    testImplementation ("junit:junit:4.13.2")
+    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation ("org.robolectric:robolectric:4.11.1")
+
+    // ViewModel
+    implementation ("androidx.activity:activity-ktx:1.8.2")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+
+    // Ui
     implementation("com.google.android.material:material:1.11.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //Images
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
+    //Hilt
+    implementation ("com.google.dagger:hilt-android:2.50")
+
+    // Coroutines
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
+    testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
+
+    // Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    //OkHttp
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation ("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
+
+    //Gson
+    implementation ("com.google.code.gson:gson:2.10.1")
+
+    //Room
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.room:room-paging:2.6.1")
 }
