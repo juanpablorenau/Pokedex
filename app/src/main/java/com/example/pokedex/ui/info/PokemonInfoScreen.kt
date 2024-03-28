73c3154e7c4facfc5dc26d083f2675af875072e2
jrenau <jrenau@mercadona.es>
2024-03-28 15:01:26 +0100

    feat/add_splash_screen: Add Status bar

--- app/src/main/java/com/example/pokedex/ui/info/PokemonInfoScreen.kt
+++ app/src/main/java/com/example/pokedex/ui/info/PokemonInfoScreen.kt
@@ -2,7 +2,9 @@ package com.example.pokedex.ui.info
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableIntState
@@ -12,12 +14,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
@@ -27,6 +31,7 @@ import com.example.pokedex.R
 import com.example.pokedex.theme.Black
 import com.example.pokedex.utils.getDominantColor
 import com.example.pokedex.utils.getViewModel
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @Composable
 fun PokemonInfoScreen(navController: NavHostController, name: String) {
@@ -87,8 +92,10 @@ fun SuccessScreen(
     pokemonInfo: PokemonInfo,
     setErrorState: (error: String) -> Unit,
 ) {
-
+    val systemUiController = rememberSystemUiController()
     val dominantColor = remember { mutableIntStateOf(Black.value.toInt()) }
+
+    systemUiController.setSystemBarsColor(color = Color(dominantColor.intValue))
 
     Scaffold(
         topBar = { TopBar(navController, dominantColor) },
@@ -96,7 +103,7 @@ fun SuccessScreen(
             Content(
                 padding = padding,
                 pokemonInfo = pokemonInfo,
-                imageColor = dominantColor.intValue,
+                dominantColor = dominantColor.intValue,
                 onSuccess = { result -> dominantColor.intValue = getDominantColor(result) },
                 onError = { error -> setErrorState(error) },
             )
@@ -132,31 +139,131 @@ fun TopBar(navController: NavHostController, dominantColor: MutableIntState) {
 fun Content(
     padding: PaddingValues,
     pokemonInfo: PokemonInfo,
-    imageColor: Int,
+    dominantColor: Int,
     onSuccess: (result: SuccessResult) -> Unit,
     onError: (error: String) -> Unit,
 ) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = padding.calculateTopPadding())
-            .clip(
-                RoundedCornerShape(
-                    topStart = 0.dp, topEnd = 0.dp, bottomStart = 48.dp, bottomEnd = 48.dp
-                )
-            ),
-        elevation = CardDefaults.cardElevation(defaultElevation = 16.dp),
-        colors = CardDefaults.cardColors(containerColor = Color(imageColor)),
-        shape = RectangleShape,
-    ) {
-        AsyncImage(
+    Column {
+        Card(
             modifier = Modifier
                 .fillMaxWidth()
-                .aspectRatio(1.75f),
-            model = pokemonInfo.url,
-            contentDescription = "Pokemon image",
-            onError = { error -> onError(error.result.toString()) },
-            onSuccess = { result -> onSuccess(result.result) }
+                .padding(
+                    top = padding.calculateTopPadding(),
+                    bottom = padding.calculateBottomPadding()
+                )
+                .clip(
+                    RoundedCornerShape(
+                        topStart = 0.dp, topEnd = 0.dp, bottomStart = 48.dp, bottomEnd = 48.dp
+                    )
+                ),
+            elevation = CardDefaults.cardElevation(defaultElevation = 16.dp),
+            colors = CardDefaults.cardColors(containerColor = Color(dominantColor)),
+            shape = RectangleShape,
+        ) {
+            AsyncImage(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1.75f),
+                model = pokemonInfo.url,
+                contentDescription = "Pokemon image",
+                onError = { error -> onError(error.result.toString()) },
+                onSuccess = { result -> onSuccess(result.result) }
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState())
+        ) {
+            Text(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 16.dp),
+                text = pokemonInfo.name,
+                color = Color(dominantColor),
+                fontSize = 36.sp
+            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Chip("Veneno", dominantColor)
+                Chip("Planta", dominantColor)
+            }
+        }
+    }
+}
+
+/*@Preview(showBackground = true)
+@Composable
+fun Content() {
+
+    Column {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(
+                    RoundedCornerShape(
+                        topStart = 0.dp, topEnd = 0.dp, bottomStart = 48.dp, bottomEnd = 48.dp
+                    )
+                ),
+            elevation = CardDefaults.cardElevation(defaultElevation = 16.dp),
+            colors = CardDefaults.cardColors(containerColor = Color(Black.value)),
+            shape = RectangleShape,
+        ) {
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1.75f),
+                painter = painterResource(id = R.drawable.ic_pokemons),
+                contentDescription = "Pokemon image",
+
+                )
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState())
+        ) {
+            Text(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                text = "Bulbasaur",
+                color = Color.Black
+            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Chip("Veneno", Red.toArgb())
+                Chip("Planta", Red.toArgb())
+            }
+
+        }
+    }
+}*/
+
+@Composable
+private fun Chip(type: String, color: Int) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .shadow(4.dp, RoundedCornerShape(12.dp)),
+        shape = RoundedCornerShape(12.dp),
+        colors = CardDefaults.cardColors().copy(containerColor = Color(color)),
+    ) {
+        Text(
+            text = type.uppercase(),
+            modifier = Modifier.padding(horizontal = 24.dp, vertical = 4.dp),
+            color = Color.Black,
+            style = MaterialTheme.typography.bodyMedium
         )
     }
 }
